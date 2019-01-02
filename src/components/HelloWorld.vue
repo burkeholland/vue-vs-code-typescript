bfde30aa843e208728a9c6d5dd0f83392a93f038
Burke Holland <burkeholland@Burkes-MBP.lan>
2019-01-02 08:53:17 -0600

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -37,6 +37,9 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+  created() {
+    console.log('created');
+  }
 }
 </script>
 
